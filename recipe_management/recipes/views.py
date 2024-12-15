28e77aee499b6c7be8ebdf386f5be459a459b279
Sharon Nzaya <45424644+Nzaya@users.noreply.github.com>
2024-12-15 17:05:36 +0300

    Added register and login html templates

--- recipe_management/recipes/views.py
+++ recipe_management/recipes/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+def register_view(request):
+    return render(request, 'registration/register.html')
+
+def login_view(request):
+    return render(request, 'registration/login.html')
+
