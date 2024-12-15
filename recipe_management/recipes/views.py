b52530fe27f28b8fa2921c114d3c7b77c9518046
Sharon Nzaya <45424644+Nzaya@users.noreply.github.com>
2024-12-15 19:19:06 +0300

    Authentication process completed

--- recipe_management/recipes/views.py
+++ recipe_management/recipes/views.py
@@ -1,8 +1,41 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login
+from django.http import JsonResponse
+import json
+from .forms import RegisterForm  
+from django.views.decorators.csrf import csrf_exempt  # Exempt token while using postman
 
-def register_view(request):
+# Registration View
+@csrf_exempt
+def register(request):
+    if request.method == 'POST':
+        # Parse JSON data from the request body
+        data = json.loads(request.body)
+        username = data.get('username')
+        email = data.get('email')
+        password = data.get('password')
+        
+        # Create a new user
+        user = User.objects.create_user(username=username, email=email, password=password)
+        user.save()
+        
+        return JsonResponse({"message": "User created successfully!"}, status=201)
     return render(request, 'registration/register.html')
 
+# Login View
+@csrf_exempt
 def login_view(request):
+    if request.method == 'POST':
+        # Parse JSON data from the request body
+        data = json.loads(request.body)
+        username = data.get('username')
+        password = data.get('password')
+        
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return JsonResponse({"message": "Login successful!"}, status=200)
+        else:
+            return JsonResponse({"message": "Invalid credentials!"}, status=400)
     return render(request, 'registration/login.html')
-
